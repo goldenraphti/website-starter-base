cfec562b10f977b9d281846b33a90a6e141812b2
Raphaël Ferrand <ferrand@protonmail.ch>
2018-06-11 18:40:45 +0200

    add js and css files

---
+++ js/app.js
@@ -0,0 +1,26 @@
+// Whole-script strict mode syntax
+'use strict';
+
+
+// execute immediately the function once DOM is ready, but impossible to reach datas inside from the outside
+(function() {
+    
+    let model = {
+        
+    };
+    
+    let octopus = {
+        
+        init : function() {
+            
+        },
+        
+    };
+    
+    let view = {
+        
+    };
+    
+    octopus.init();
+    
+})();
